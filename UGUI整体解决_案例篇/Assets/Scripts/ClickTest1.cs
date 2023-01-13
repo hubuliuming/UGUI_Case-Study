96e8035b112962323bd3791f31f3e145e8e155a0
Y <97585120+hubuliuming@users.noreply.github.com>
2023-01-13 14:59:26 +0800

    Update: Init local file

---
+++ UGUI整体解决_案例篇/Assets/Scripts/ClickTest1.cs
@@ -0,0 +1,22 @@
+/****************************************************
+    文件：ClickTest1.cs
+    作者：Y
+    邮箱: 916111418@qq.com
+    日期：#CreateTime#
+    功能：Nothing
+*****************************************************/
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickTest1 : MonoBehaviour ,IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click");
+    }
+
+    
+}
+
